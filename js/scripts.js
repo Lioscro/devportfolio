af7e16a86ecfd7083b24ea67b85d5386cd388514
Joseph Min <phoenixter96@gmail.com>
2019-07-22 16:12:53 -0400

    Initial mrege (#1)
    * fixed a typo.
    
    * Update README.md
    
    * Add linkedIn to list of social networks
    
     * Addressed Issue #47
    
    * Updated JS dependencies
    
    * Fixed Gulp watch task that would not listen to any file changes
    
    * Fixed infinite loop when updating JS code
    
    * Update .gitignore
    
    * initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -51,7 +51,7 @@
     });
 
     // Create timeline
-    $('#experience-timeline').each(function() {
+    $('.timeline-timeline').each(function() {
 
         $this = $(this); // Store reference to this
         $userContent = $this.children('div'); // user content
